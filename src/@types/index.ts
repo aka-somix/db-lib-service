67f6c88c8cdd8915462a120b0e1ebd7cc6b7e074
Somix <somix.land@null.net>
2024-06-02 23:34:09 +0200

    :sparkles: add customer by id

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,4 +1,5 @@
 export interface Customer{
+  id: string;
   name: string;
   surname: string;
   phoneNumber: string;
--- src/services/customerSerivce.ts
+++ src/services/customerSerivce.ts
@@ -1,22 +1,42 @@
+import { eq } from "drizzle-orm";
 import { Customer } from "../@types";
 import {pgClient} from '../database/client';
-// import {customers} from 'db-lib-database-mgmt/src';
+import {customers} from 'db-lib-database-mgmt';
 
 export class CustomerService {
 
   public static async getCustomers(limit?: number): Promise<Customer[]> {
 
-    // const data = await pgClient.select().from(customers).limit(limit ?? 10);
+    const data = await pgClient.select().from(customers).limit(limit ?? 10);
 
-    // return data.map((c) => {
-    //   return {
-    //     name: c.name ?? "-",
-    //     surname: c.surname ?? "-",
-    //     phoneNumber: c.phone ?? "-"
-    //   }
-    // })
+    return data.map((c) => {
+      return {
+        id: c.id.toString(),
+        name: c.name ?? "-",
+        surname: c.surname ?? "-",
+        phoneNumber: c.phone ?? "-"
+      }
+    })
+  }
 
-    return []
+  public static async getCustomersFromId(id: string): Promise<Customer | null> {
+
+    const data = await pgClient
+      .select()
+      .from(customers)
+      .where(eq(customers.id, parseInt(id)))
+      .limit(1);
+
+    if (data.length === 0) return null;
+
+    const customer = data[0];
+
+    return {
+      id: customer.id.toString(),
+      name: customer.name ?? "-",
+      surname: customer.surname ?? "-",
+      phoneNumber: customer.phone ?? "-"
+    }
   }
 
 }
