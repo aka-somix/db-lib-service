1643ed0f94a0f2bee7e99bd559227e3d9fe1d6f8
Somix <somix.land@null.net>
2024-06-02 18:55:51 +0200

    :sparkles: add client for drizzle

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const DB_HOST = process.env.DB_HOST ?? "127.0.0.1"
+export const DB_PORT = parseInt(process.env.DB_PORT ?? "5432")
+export const DB_USER = process.env.DB_USER ?? "postgres"
+export const DB_PSW = process.env.DB_PSW ?? "password"
+export const DB_NAME = process.env.DB_PSW ?? "postgres"
---
+++ src/database/client.ts
@@ -0,0 +1,13 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import { DB_HOST, DB_NAME, DB_PORT, DB_PSW, DB_USER } from "../config";
+
+const pool = new Pool({
+  host: DB_HOST,
+  port: DB_PORT,
+  user: DB_USER,
+  password: DB_PSW,
+  database: DB_NAME,
+});
+
+const pgClient = drizzle(pool);
